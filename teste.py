365a62aa4510c24e9ee6dbdf340eb90cdfc179cb
Vitoria Damasceno <83039172+vitoriadaamasceno@users.noreply.github.com>
2022-09-08 00:25:06 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime,date
-from models import Lembrete
-data='31/12/2021'
-data_atual= datetime.date.today().strftime('%d/%m/%Y')
-data_formatada= datetime.strptime(data, '%d/%m/%Y').date()
-if data_formatada >= data_atual:
-   print('é')
-else: 
-    print('não é')
