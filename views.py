6682fac7b21b523ae9242d82f4fc440a09cfe57a
Vitoria Damasceno <83039172+vitoriadaamasceno@users.noreply.github.com>
2022-09-09 15:52:53 -0300

    validação de entradas

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask ,render_template,request,redirect,session,flash, url_for
 from api import app,db
@@ -18,27 +19,38 @@ def index():
 def criar():
     nome= request.form['nome'] 
     data= request.form['data']
-    
-    #Formatação de datas
-    data_hoje= date.today().strftime('%Y-%m-%d')
-    data_hoje_formatada=datetime.strptime(data_hoje, '%Y-%m-%d').date()
-    data_forms= datetime.strptime(data, '%Y-%m-%d').date()
 
-    #Validando entradas
+    #Validando entradas vazias
     if nome != '':
         pass
     else:
         flash('Todos os campos devem está preenchidos!')
         return redirect(url_for('index'))
+
+    if data != '':
+        pass
+    else:
+        flash('Todos os campos devem está preenchidos!')
+        return redirect(url_for('index'))
+
+    #Formatação de datas
+    data_hoje= date.today().strftime('%Y-%m-%d')
+    data_hoje_formatada=datetime.strptime(data_hoje, '%Y-%m-%d').date()
+    data_forms= datetime.strptime(data, '%Y-%m-%d').date()
+
     #datas existentes
     datas= Lembrete.query.filter_by(data=data).first()
     if datas:
+        
+        
         novo_lembrete=Lembrete(nome=nome,data=data)
         db.session.add(novo_lembrete)
         db.session.commit() #salva a informação no banco
-        flash('Lembrete com data existente,enviado com sucesso!')
+        flash('Lembrete com data existente enviado com sucesso!')
         return redirect(url_for('index'))
-    
+
+    #validando datas maiores
+
     if data_forms >= data_hoje_formatada:
         novo_lembrete=Lembrete(nome=nome, data=data)
         db.session.add(novo_lembrete) 
@@ -50,6 +62,7 @@ def criar():
         return redirect(url_for('index'))
     
 
+    
 
 @app.route('/deletar/<int:id>')
 def deletar(id):
